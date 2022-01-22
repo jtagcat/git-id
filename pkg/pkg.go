06d51e413edcfed4a123cf35caf4a1453e15c697
jtagcat <git-12dbd862@jtag.cat>
2022-01-22 22:11:43 +0200

    pkg: refactor; add WriteFileExisting()

--- pkg/pkg.go
+++ pkg/pkg.go
@@ -6,16 +6,22 @@ import (
 	"os"
 )
 
-// os.Rename() if dst not exist
-//
-// use: errors.Is(err, fs.ErrExist)
-func RenameNoOverwrite(src, dst string) error {
-	_, err := os.Stat(dst)
+// os.Rename(), but newpath:fs.ErrExist
+func RenameNoOverwrite(oldpath, newpath string) error {
+	_, err := os.Stat(newpath)
 	if err == nil {
 		return fs.ErrExist
 	}
 	if errors.Is(err, os.ErrNotExist) {
-		return os.Rename(src, dst)
+		return os.Rename(oldpath, newpath)
 	}
 	return err
+}
+
+// os.WriteFile, but fs.ErrExist
+func WriteFileExisting(name string, data []byte) error {
+	if _, err := os.Stat(name); err == nil {
+		return os.WriteFile(name, data, 0000)
+	}
+	return fs.ErrExist
 }
