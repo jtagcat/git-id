ec679be64941972cfcfc7bd9a1935a55e3d917b3
jtagcat <git-12dbd862@jtag.cat>
2022-02-06 22:20:39 +0200

    getBetweenHeaders now uses root objects instead of comms

--- pkg/encoding/ssh_config/layer2b.go
+++ pkg/encoding/ssh_config/layer2b.go
@@ -4,36 +4,43 @@ import (
 	"strings"
 )
 
-// splits []RawTopLevel to up to 3, based on start/end header (comment). 2nd is what you usually want.
+// splits []RawTopLevel to up to 3, based on start/end header key-values (headers are usually comments, use xkeys).
+// 2nd return is what you usually want.
+//
 // Only the first match will be considered;
 // header of default value ("") will be ignored.
-func GetBetweenHeaders(cfg []RawTopLevel, headers, eofHeaders []string) (beforeHeader, betweenHeaders, afterHeader []RawTopLevel) {
-	headerMap := make(map[string]bool)
-	for _, h := range headers {
-		headerMap[strings.ToLower(h)] = false
+func GetBetweenHeaders(cfg []RawTopLevel, startHeaders, eofHeaders []RawTopLevel) (beforeHeader, betweenHeaders, afterHeader []RawTopLevel) {
+	startHeaderMap := make(map[string][]RawValue)
+	for _, h := range startHeaders {
+		startHeaderMap[strings.ToLower(h.Key)] = h.Values
 	}
-	eofHeaderMap := make(map[string]bool)
+	eofHeaderMap := make(map[string][]RawValue)
 	for _, h := range eofHeaders {
-		eofHeaderMap[strings.ToLower(h)] = false
+		eofHeaderMap[strings.ToLower(h.Key)] = h.Values
 	}
 
 	var state int
-	if len(headers) == 0 {
+	if len(startHeaders) == 0 {
 		state = 1
 	}
 	for _, line := range cfg {
 		switch state {
 		case 0: // before header
-			if _, ok := headerMap[strings.ToLower(line.Comment)]; ok {
-				// found first header
-				state = 1
-				continue
+			if values, ok := startHeaderMap[strings.ToLower(line.Key)]; ok { // key matches
+				if valuesMatch(line.Values, values) {
+					// found a starting header
+					state = 1
+					continue
+				}
 			}
 			beforeHeader = append(beforeHeader, line)
 		case 1: // during header
-			if _, ok := eofHeaderMap[strings.ToLower(line.Comment)]; ok {
-				state = 2
-				continue
+			if values, ok := eofHeaderMap[strings.ToLower(line.Key)]; ok {
+				if valuesMatch(line.Values, values) {
+					// found an eofHeader
+					state = 2
+					continue
+				}
 			}
 			betweenHeaders = append(betweenHeaders, line)
 		case 2: // after eofHeader
@@ -42,6 +49,20 @@ func GetBetweenHeaders(cfg []RawTopLevel, headers, eofHeaders []string) (beforeH
 	}
 	return beforeHeader, betweenHeaders, afterHeader
 }
+
+func valuesMatch(a, b []RawValue) bool { // similar to EqualFoldSlice
+	if len(a) != len(b) {
+		return false
+	}
+	for i, v := range a {
+		if !strings.EqualFold(v.Value, b[i].Value) {
+			return false
+		}
+	}
+	return true
+}
+
+//
 
 //
 
@@ -56,8 +77,6 @@ func GetBetweenHeaders(cfg []RawTopLevel, headers, eofHeaders []string) (beforeH
 // request: specific key(s) under a specified host(s) jq: '.[]host | | select(.key == $key)'
 // returns slice of matches (rawKeyword)
 // not intended: using the returned values directly for writing (use nth)
-// func MatchingHosts(cfg []RawTopLevel, hostkey, hostvalue, key string, keyIsComment bool) {
-// 	for _, line := range cfg {
-// 		if strings.EqualFold(line.Key) {
-// 	}
+// MATCH not implemeneted
+// func MatchingHostsTrees(cfg []RawTopLevel, hostkey, hostvalue, key string, keyIsComment bool) {
 // }
