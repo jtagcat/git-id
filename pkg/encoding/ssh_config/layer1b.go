b8808759b8e21cd9551e667ce92e60c560534ec9
jtagcat <git-12dbd862@jtag.cat>
2022-02-06 12:42:28 +0200

    refactor: decodeXKeys under decodeKeys + more

--- pkg/encoding/ssh_config/layer1b.go
+++ pkg/encoding/ssh_config/layer1b.go
@@ -1,5 +1,6 @@
 package ssh_config
 
+// 1b: the raw, no-typing way
 import (
 	"bufio"
 	"fmt"
@@ -8,15 +9,19 @@ import (
 	"strings"
 )
 
-// 1b: the raw, no-typing way
-
-func DecodeToRaw(data io.Reader) ([]RawTopLevel, error) {
+// xkeys: Custom keys nested inside comments.
+// rootXKeys: list of root-level xkeys; bool: may have children (recommend default: true)
+// subXKeys: list of sub-level xkeys; bool means nothing
+func DecodeToRaw(data io.Reader, rootXKeyMap map[string]bool, subXKeyMap map[string]bool) ([]RawTopLevel, error) {
 	var deep bool         // under a host or match
 	var cfg []RawTopLevel // tree is flusehd to cfg
 	var tree RawTopLevel  // current level
 
 	keywordType := reflect.TypeOf(Keywords{})
-	keywordsTotal := keywordType.NumField()
+	keywordKMap := make(map[string]bool)
+	for i := 0; i < keywordType.NumField(); i++ {
+		keywordKMap[strings.ToLower(keywordType.Field(i).Name)] = false
+	}
 
 	var prevLineComment []string // buffer f
 
@@ -30,65 +35,91 @@ func DecodeToRaw(data io.Reader) ([]RawTopLevel, error) {
 			return cfg, err
 		}
 
-		switch line.Key {
-		case "":
-			if !deep { // TLD
-				if i != 1 { // flush previous [macro C]
+		// xkey
+		var rootXKey, rootXKeyMayHaveChildren, subXKey bool
+		if line.Key == "" && line.Comment != "" {
+			locaseCmt := strings.ToLower(line.Comment)
+
+			for rk, mayHaveChildren := range rootXKeyMap {
+				if strings.HasPrefix(locaseCmt, rk) {
+					rootXKey = true
+					rootXKeyMayHaveChildren = mayHaveChildren
+				}
+			}
+			for sk, _ := range subXKeyMap {
+				if strings.HasPrefix(locaseCmt, sk) {
+					subXKey = true
+				}
+			}
+
+			line, err = decodeLine(strings.ToValidUTF8(line.Comment, ""))
+			if err != nil {
+				err = fmt.Errorf("while parsing xkey on line %d: %w", i, err)
+			}
+		}
+
+		if line.Key == "" {
+			if !deep { // already at TLD level
+				if i != 1 { // flush previous tree
 					cfg = append(cfg, tree)
 				}
 				tree = RawTopLevel{Comment: line.Comment} // may be an empty line as well; empty comments are trimmed
-			} else { // in a tree, buffering to see whether we hit a new TLD or subkey (in the same tree) first.
+			} else { // in a tree, buffering to check afterwards whether we hit a subkey (including comment in the tree) or a new tree / EOF
 				prevLineComment = append(prevLineComment, line.Comment)
 			}
-		case "Host", "Match", "Include":
-			if line.Key == "Include" {
+			continue // would-be switch statement (emulating it), if not for the xkey check
+		}
+
+		locaseKey := strings.ToLower(line.Key)
+		if rootXKey || locaseKey == "host" || locaseKey == "match" || locaseKey == "include" {
+			if locaseKey == "include" || rootXKey && !rootXKeyMayHaveChildren {
 				deep = false
 			} else {
 				deep = true
 			}
-			if i != 1 {
+			if i != 1 { // handle attaching comments to trees [macro A]
 				for _, c := range prevLineComment { // i != 1 anyway
 					if c == "" {
-						break
+						break // empty line (unless the next key is subkey) moves the line, and subsequent comments to root
 					}
+					// flush comment to previous tree
 					tree.Children = append(tree.Children, RawKeyword{Comment: c})
-					prevLineComment = prevLineComment[1:]
+					prevLineComment = prevLineComment[1:] // remove parsed from buffer
 				}
-				cfg = append(cfg, tree)
-				for _, c := range prevLineComment { // continue as !deep
+				cfg = append(cfg, tree)             // flush previous tree (ALSO happens when no comments in buffer)
+				for _, c := range prevLineComment { // continue as !deep, at root level
 					tree = RawTopLevel{Comment: c}
 					cfg = append(cfg, tree)
 				}
-				prevLineComment = []string{}
+				prevLineComment = []string{} // clear buffer
 			}
+			// create a new tree (previous already flushed)
 			tree = RawTopLevel{line.Key, line.Values, line.Comment, line.EncodingKVSeperatorIsEquals, []RawKeyword{}}
-		default:
-			if !deep {
-				return cfg, fmt.Errorf("while parsing line %d: %w: TLD %s", i, ErrInvalidKeyword, line.Key)
-			}
+			continue // emulating switch statement
+		}
 
-			// flush comments
-			for _, c := range prevLineComment { // i != 1 anyway
-				tree.Children = append(tree.Children, RawKeyword{Comment: c})
-			}
-			prevLineComment = []string{}
+		// any other key will be subkey (switch: default)
+		if !deep {
+			return cfg, fmt.Errorf("while parsing line %d: %w: TLD %s", i, ErrInvalidKeyword, line.Key)
+		}
 
-			// basic 'does key exist'
-			var exists bool
-			for i := 0; i < keywordsTotal; i++ {
-				if strings.EqualFold(keywordType.Field(i).Name, line.Key) {
-					exists = true
-					break
-				}
-			}
-			if !exists {
+		// flush comments
+		for _, c := range prevLineComment { // i != 1 anyway
+			tree.Children = append(tree.Children, RawKeyword{Comment: c})
+		}
+		prevLineComment = []string{}
+
+		if !subXKey {
+			// basic 'does (non-x)key exist'
+			if _, ok := keywordKMap[locaseKey]; !ok {
 				return cfg, fmt.Errorf("while parsing line %d: %w", i, ErrInvalidKeyword)
 			}
-
-			tree.Children = append(tree.Children, line)
 		}
+
+		tree.Children = append(tree.Children, line)
 	}
-	for _, c := range prevLineComment { // add comments before newline to last tree
+
+	for _, c := range prevLineComment { // flush comments before newline to last tree [macro A]
 		if c == "" {
 			break
 		}
